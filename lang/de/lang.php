a65ed92740fab4da611c35352e90eba7c5ccc9e9
Andreas Gohr <gohr@cosmocode.de>
2012-08-23 14:00:51 +0200

    german translation added

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * German language file for addomain plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+
+$lang['domain'] = 'Anmelde-Domäne';
+
+
+
+//Setup VIM: ex: et ts=4 :
